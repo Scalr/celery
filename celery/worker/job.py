54fdbdcd01ed22763d9c1dec01d19e9f342227db
Hennadii Stas <uty.arrghhh@gmail.com>
2016-12-05 16:22:35 +0200

    FAM-390 amqp_user_id is now set in delivery info instead of headers

--- celery/worker/job.py
+++ celery/worker/job.py
@@ -179,20 +179,20 @@ class Request(object):
         if message:
             delivery_info = message.delivery_info or {}
             properties = message.properties or {}
+            body_delivery_info = {
+                'exchange': delivery_info.get('exchange'),
+                'routing_key': delivery_info.get('routing_key'),
+                'priority': properties.get(
+                    'priority', delivery_info.get('priority')),
+                'redelivered': delivery_info.get('redelivered'),
+            }
             if properties.get('user_id'):
-                message.headers['amqp_user_id'] = properties.get('user_id')
+                body_delivery_info['amqp_user_id'] = properties.get('user_id')
             body.update({
                 'headers': message.headers,
                 'reply_to': properties.get('reply_to'),
                 'correlation_id': properties.get('correlation_id'),
-                'delivery_info': {
-                    'exchange': delivery_info.get('exchange'),
-                    'routing_key': delivery_info.get('routing_key'),
-                    'priority': properties.get(
-                        'priority', delivery_info.get('priority')),
-                    'redelivered': delivery_info.get('redelivered'),
-                }
-
+                'delivery_info': body_delivery_info,
             })
         else:
             body.update(DEFAULT_FIELDS)
