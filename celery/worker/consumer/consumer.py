f0eb119eb93222215f56dcf5965d4c754f6b5c36
Serhii Babak <s.babak@scalr.com>
2020-10-23 17:34:34 +0300

    SCALRCORE-17315 handle BrokenPipeError

--- celery/worker/consumer/consumer.py
+++ celery/worker/consumer/consumer.py
@@ -233,6 +233,9 @@ class Consumer(object):
             while self._pending_operations:
                 try:
                     self._pending_operations.pop()()
+                except BrokenPipeError:
+                    # re-raise this error to allow a consumer to reconnect
+                    raise
                 except Exception as exc:  # pylint: disable=broad-except
                     logger.exception('Pending callback raised: %r', exc)
 
